e827e0a128fe7a9d36bc854168ffc99193e4a2ba
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-22 17:26:25 +0600

    User put API created

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 //DB info
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oo3aa.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bm8ub.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -21,14 +20,18 @@ const run = async () => {
   try {
     //DB client connect
     await client.connect();
+    //Collections
+    const usersCollection = client.db("paint_tools").collection("users");
+    //Put users info
+    app.put("/put-user", async (req, res) => {
+      const usersData = req.body;
+      const result = await usersCollection.insertOne(usersData);
+      res.send(result);
+    });
   } finally {
     //Connection continue
   }
 };
 run().catch(console.dir);
-//Test
-app.get("/", (req, res) => {
-  res.send("Server is running on port 5000");
-});
 // Server listning
 app.listen(port, () => console.log("Server is running on", port));
