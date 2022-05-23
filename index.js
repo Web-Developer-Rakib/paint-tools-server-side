3ec78bc9691a9134c39b809282bd20934fa2ac3b
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-23 23:21:40 +0600

    Single product get API created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 //Middlewares
@@ -73,6 +73,20 @@ const run = async () => {
       const cursor = usersCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    });
+    //Get all Products
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //Get a single Product
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
     });
   } finally {
     //Connection continue
