d783dd71ff405305554253fc301a272c14347a2b
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-24 15:25:13 +0600

    Payment intent API created

--- index.js
+++ index.js
@@ -4,9 +4,26 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 //Middlewares
 app.use(cors());
 app.use(express.json());
+
+//Verify JWT
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 //DB info
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bm8ub.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -68,7 +85,7 @@ const run = async () => {
       const result = await productsCollection.insertOne(productsData);
       res.send(result);
     });
-    //Get Users
+    //Get all Users
     app.get("/users", async (req, res) => {
       const query = {};
       const cursor = usersCollection.find(query);
@@ -94,6 +111,48 @@ const run = async () => {
       const ordersData = req.body;
       const result = await ordersCollection.insertOne(ordersData);
       res.send(result);
+    });
+    //Get all orders info
+    app.get("/orders", async (req, res) => {
+      const query = {};
+      const cursor = ordersCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //Get a single order info
+    app.get("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await ordersCollection.findOne(query);
+      res.send(order);
+    });
+    //Create payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const product = req.body;
+      const price = product.totalPrice;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({ clientSecret: paymentIntent.client_secret });
+    });
+
+    // Update payment status
+    app.patch("/update-payment-status/:id", verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+        },
+      };
+      const updatedPaymentStatus = await ordersCollection.updateOne(
+        filter,
+        updatedDoc
+      );
+      res.send(updatedPaymentStatus);
     });
   } finally {
     //Connection continue
