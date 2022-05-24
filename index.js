c3baddc86897d62c09a666afd0e39e7fde2d1d6e
WebDeveloperApolo <rakibodesk7@gmail.com>
2022-05-24 15:55:51 +0600

    Product delete API created

--- index.js
+++ index.js
@@ -154,6 +154,13 @@ const run = async () => {
       );
       res.send(updatedPaymentStatus);
     });
+    // Delete product
+    app.delete("/delete-product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await productsCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     //Connection continue
   }
